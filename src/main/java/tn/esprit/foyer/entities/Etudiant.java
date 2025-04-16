a12e2d87960b7edf543a73800137d1629a0eb580
ayouta2002 <eya.benslimen@esprit.tn>
2025-04-16 21:32:46 +0100

    correction test unitaire

--- src/main/java/tn/esprit/foyer/entities/Etudiant.java
+++ src/main/java/tn/esprit/foyer/entities/Etudiant.java
@@ -1,6 +1,5 @@
 package tn.esprit.foyer.entities;
 
-
 import jakarta.persistence.*;
 import lombok.*;
 import java.io.Serializable;
@@ -12,49 +11,39 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true) // <- active le contrôle manuel
 @ToString
-@RequiredArgsConstructor
 public class Etudiant implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idEtudiant; // Clé primaire
-    @NonNull
+    private Long idEtudiant;
+
+    @EqualsAndHashCode.Include
+    private Long cin; // ⚠️ identifiant unique logique
+
     private String nomEt;
-    @NonNull
-     String prenomEt;
-     Long cin;
-     String ecole;
-     LocalDate dateNaissance;
-
-
-
-
-
-
-
-
-
+    private String prenomEt;
+    private String ecole;
+    private LocalDate dateNaissance;
 
     @OneToMany(mappedBy = "etudiant")
     List<Tache> taches;
+
     Float montantInscription;
+
     @Enumerated(EnumType.STRING)
     TypeEtudiant typeEtudiant;
 
-    @ManyToMany(mappedBy = "etudiants",fetch = FetchType.EAGER)
+    @ManyToMany(mappedBy = "etudiants", fetch = FetchType.EAGER)
     List<Reservation> reservations;
 
     @OneToOne
     Tache tache;
+
     public Etudiant(String nomEt, String prenomEt, String ecole) {
         this.nomEt = nomEt;
         this.prenomEt = prenomEt;
         this.ecole = ecole;
     }
-
-
-
-
-
 }
-
