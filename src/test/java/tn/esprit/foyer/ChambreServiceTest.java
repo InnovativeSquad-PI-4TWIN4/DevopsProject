1ea800477a2a4cf81d39b0c6170fe67cba3b0f39
mouadhfersi25 <mouadh.fersi@esprit.tn>
2025-04-17 20:50:20 +0100

    test unitaires updates

--- src/test/java/tn/esprit/foyer/ChambreServiceTest.java
+++ src/test/java/tn/esprit/foyer/ChambreServiceTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class) // JUnit + Mockito
-public class ChambreServiceTest {
+ class ChambreServiceTest {
 
     @Mock  // Mockito : simulateur de ChambreRepository
     private ChambreRepository chambreRepository;
