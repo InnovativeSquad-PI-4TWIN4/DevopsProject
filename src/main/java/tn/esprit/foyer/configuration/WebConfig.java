7f5464b299073e8ebba91fd0f21c43f052006685
firas090 <146679669+firas090@users.noreply.github.com>
2025-04-17 21:21:29 +0100

    Update WebConfig.java

--- src/main/java/tn/esprit/foyer/configuration/WebConfig.java
+++ src/main/java/tn/esprit/foyer/configuration/WebConfig.java
@@ -5,9 +5,9 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
