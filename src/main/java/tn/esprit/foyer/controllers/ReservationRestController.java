14c7ee6f0d3720fe5d913c85fcd64de2dbc66e55
ayouta2002 <eya.benslimen@esprit.tn>
2025-04-17 22:51:43 +0100

    commit 5

--- src/main/java/tn/esprit/foyer/controllers/ReservationRestController.java
+++ src/main/java/tn/esprit/foyer/controllers/ReservationRestController.java
@@ -40,14 +40,14 @@ public class ReservationRestController {
 
     // http://localhost:8089/foyer/reservation/retrieve-reservation/8
     @GetMapping("/retrieve-reservation/{reservationId}")
-    @ResponseBody
+
     public Reservation retrieveReservation(@PathVariable("reservationId") Long reservationId) {
         return reservationService.retrieveReservation(reservationId);
     }
 
     // http://localhost:8089/foyer/reservation/add-reservation
     @PostMapping("/add-reservation")
-    @ResponseBody
+
     public Reservation addReservation(@RequestBody Reservation r) {
         Reservation reservation= reservationService.addReservation(r);
         return reservation;
@@ -55,28 +55,28 @@ public class ReservationRestController {
 
     // http://localhost:8089/foyer/reservation/update-reservation
     @PutMapping("/update-reservation")
-    @ResponseBody
+
     public Reservation updateReservation(@RequestBody Reservation r) {
         Reservation reservation= reservationService.updateReservation(r);
         return reservation;
     }
     // http://localhost:8089/foyer/reservation/removeReservation
     @DeleteMapping("/removeReservation/{idReservation}")
-    @ResponseBody
+
     public void removeReservation(@PathVariable("idReservation") Long idReservation) {
         reservationService.removeReservation(idReservation);
     }
 
          // http://localhost:8089/foyer/reservation/ajouterReservationEtAssignerAChambreEtAEtudiant/15/8453621
          @PostMapping("/ajouterReservationEtAssignerAChambreEtAEtudiant/{numChambre}/{cin}")
-         @ResponseBody
+
          public Reservation ajouterReservationEtAssignerAChambreEtAEtudiant(@RequestBody Reservation r,@PathVariable("numChambre") Long numChambre,@PathVariable("cin") long cin) {
              Reservation reservation= reservationService.ajouterReservationEtAssignerAChambreEtAEtudiant(r,numChambre,cin);
              return reservation;
          }
     // http://localhost:8089/foyer/reservation/getReservationParAnneeUniversitaire/2021-01-01/2021-12-31
     @GetMapping("/getReservationParAnneeUniversitaire/{dateDebut}/{dateFin}")
-    @ResponseBody
+
     public List<Reservation> getReservationParAnneeUniversitaire(@PathVariable("dateDebut") LocalDate dateDebut,@PathVariable("dateFin") LocalDate dateFin) {
         return reservationService.getReservationParAnneeUniversitaire(dateDebut,dateFin);
     }
