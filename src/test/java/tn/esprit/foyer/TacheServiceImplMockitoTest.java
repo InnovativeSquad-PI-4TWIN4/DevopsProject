ecf73af1cb0accb58f675ab338519ee5ed8273c0
firas090 <benromdhane.firas@esprit.tn>
2025-04-17 22:12:14 +0100

    code changes

--- src/test/java/tn/esprit/foyer/TacheServiceImplMockitoTest.java
+++ src/test/java/tn/esprit/foyer/TacheServiceImplMockitoTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
- class TacheServiceImplMockitoTest {
+public class TacheServiceImplMockitoTest {
 
     @Mock   //Permet de créer un mock (simulation) d'une dépendance.
     private TacheRepository tacheRepository;
