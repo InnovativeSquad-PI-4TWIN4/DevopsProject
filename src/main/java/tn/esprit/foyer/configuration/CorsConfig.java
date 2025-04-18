ec5119a38edd579d3e5232df0f5473c83637f693
mouadhfersi25 <mouadh.fersi@esprit.tn>
2025-04-18 03:06:55 +0100

    updatecode

---
+++ src/main/java/tn/esprit/foyer/configuration/CorsConfig.java
@@ -0,0 +1,24 @@
+package tn.esprit.foyer.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // autoriser toutes les routes
+                        // autoriser toutes les origines (ou "http://localhost:52924")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false); // mettre à true si tu gères les cookies
+            }
+        };
+    }
+}
