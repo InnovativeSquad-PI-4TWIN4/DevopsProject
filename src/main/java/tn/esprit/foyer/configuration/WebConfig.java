b7980725b997eae84f82fb80be4149765c473756
firas090 <146679669+firas090@users.noreply.github.com>
2025-04-18 12:18:25 +0100

    Update WebConfig.java

--- src/main/java/tn/esprit/foyer/configuration/WebConfig.java
+++ src/main/java/tn/esprit/foyer/configuration/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://172.28.220.156:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
